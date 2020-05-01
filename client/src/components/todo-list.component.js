de3e25ad71f8ef324f83f303545222c8e0f6e2bc
dthemg <david.montgomery@live.se>
2020-05-01 16:51:16 +0200

    Show TODO list from DB

--- client/src/components/todo-list.component.js
+++ client/src/components/todo-list.component.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+const Todo = props => (
+  <tr>
+    <td>{ props.todo.todo_description }</td>
+    <td>{ props.todo.todo_responsible }</td>
+    <td>{ props.todo.todo_priority }</td>
+    <td>
+      <Link to={"/update/" + props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+)
+
 
 
 export function TodoList() {
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:9000/getAll')
+    .then(response => {
+      setTodos(response.data)
+    })
+  }, []);
+
+  const tableBody = () => {
+    return (
+      todos.map(function(thisTodo, i) {
+        return <Todo todo={thisTodo} key={i}/>
+      })
+    )
+  }
+
   return (
-    <p>TODO-list here</p>
+    <div>
+      <h3>Todo List</h3>
+      <table
+        className="table table-striped" style={{ marginTop: 20 }}
+      >
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Responsible</th>
+            <th>Priority</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          { todos.map(function(thisTodo, i) {
+            return <Todo todo={thisTodo} key={i}/>
+          }) }
+        </tbody>
+      </table>
+    </div>
   )
 }
